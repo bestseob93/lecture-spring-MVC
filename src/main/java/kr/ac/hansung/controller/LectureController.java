990f0f4ddc19858bc7fdd6201265805ec845c972
Hwan Seob Lee <bestseob93@gmail.com>
2017-12-09 21:42:53 +0900

    lectures view added

--- src/main/java/kr/ac/hansung/controller/LectureController.java
+++ src/main/java/kr/ac/hansung/controller/LectureController.java
@@ -3,12 +3,14 @@ package kr.ac.hansung.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.ac.hansung.model.Lecture;
 import kr.ac.hansung.service.LectureService;
 
+@Controller
 public class LectureController {
 	
 	@Autowired
